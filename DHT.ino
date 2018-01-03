d0613e2465bedc5124b73cf6b65b3d513a3645cc
장보우 Bowoo Jang <codingbowoo@gmail.com>
2018-01-03 13:15:12 +0900

    Update DHT.ino

--- DHT.ino
+++ DHT.ino
@@ -23,3 +23,19 @@ void setup()
 	dht.begin();
 
 }
+
+void loop()
+{
+    //Read data and store it to variables hum and temp
+    hum = dht.readHumidity();
+    temp= dht.readTemperature();
+    //Print temp and humidity values to serial monitor
+    Serial.print("Humidity: ");
+    Serial.print(hum);
+    Serial.print(" %, Temp: ");
+    Serial.print(temp);
+    Serial.println(" Celsius");
+    delay(2000); //Delay 2 sec.
+}
+
+   
